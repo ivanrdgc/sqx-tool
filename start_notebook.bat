79652363f42f13aee4de0e2bcdf928f816478b04
Ivan Radigales Creus <ivanrdgc@gmail.com>
2026-04-21 22:29:44 +0200

    Fixed issue with notebook

--- start_notebook.bat
+++ start_notebook.bat
@@ -1,3 +1,3 @@
 @echo off
 set PY_HOME=%~dp0src\python
-"%PY_HOME%\python.exe" -m jupyter notebook --notebook-dir="%~dp0Notebooks" %*
+"%PY_HOME%\python.exe" -m notebook --notebook-dir="%~dp0Notebooks" %*
